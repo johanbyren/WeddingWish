71088ca6ad16d2643aef70228ae7212d601a1428
Johan Byren <johan.byren@gmail.com>
2025-05-01 00:40:09 +0200

    Fixed the protected route, there is now not possible to be on dashboard if not loggedin

--- packages/web/app/components/protected-route.tsx
+++ packages/web/app/components/protected-route.tsx
@@ -1,12 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '~/context/auth';
 
-export const ProtectedRoute = () => {
-  const { user, loaded } = useAuth();
+export default function ProtectedRoute() {
+  const { loggedIn, loaded } = useAuth();
 
-  if (!user && loaded) {
+  if (!loggedIn && loaded) {
     return <Navigate to="/" replace state={{ from: location.pathname }} />;
   }
 
   return <Outlet />;
-};
+}
